bd09e74c60cec859e07a603b410ebba100dbff6e
Ruthwik <140411616+Ruth-wik@users.noreply.github.com>
2025-09-13 10:19:08 -0400

    dinuziuesn

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTeleOp.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import Subsystems.MecanumDrivetrain;
+
+@TeleOp(name = "Drive For Now")
+public class MyTeleOp extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        MecanumDrivetrain drivetrain = new MecanumDrivetrain(hardwareMap);
+        GamepadEx driverOp = new GamepadEx(gamepad1);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            drivetrain.drive(
+                    driverOp.getLeftX(),
+                    -driverOp.getLeftY(),
+                    driverOp.getRightX()
+            );
+        }
+    }
+}
