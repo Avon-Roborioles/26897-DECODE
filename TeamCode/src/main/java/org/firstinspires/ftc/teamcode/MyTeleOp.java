a3781c834018c33a800a1dccbc0a9182cdfabd1e
yash <yashf1geek@gmail.com>
2025-09-27 11:36:08 -0400

    i did something

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTeleOp.java
@@ -1,15 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import Subsystems.MecanumDrivetrain;
 
 @TeleOp(name = "Drive For Now")
 public class MyTeleOp extends LinearOpMode {
+    private Motor frontLeft;
+    private Motor backLeft;
+    private Motor frontRight;
+    private Motor backRight;
+
     @Override
     public void runOpMode() {
-        MecanumDrivetrain drivetrain = new MecanumDrivetrain(hardwareMap);
+        frontLeft = new Motor(hardwareMap, "frontLeft");
+        frontRight = new Motor(hardwareMap, "frontRight");
+        backLeft = new Motor(hardwareMap, "backLeft");
+        backRight = new Motor(hardwareMap, "backRight");
+
+        MecanumDrive drivetrain = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
         GamepadEx driverOp = new GamepadEx(gamepad1);
 
         telemetry.addData("Status", "Initialized");
