db4d4471c3339a451a01dc449dd73b5bc2bdddde
yash <yashf1geek@gmail.com>
2025-10-28 15:43:39 -0400

    idk what this is but its from before break
    update my teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTeleOp.java
@@ -3,12 +3,18 @@ package org.firstinspires.ftc.teamcode;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+<<<<<<< Updated upstream
 import org.firstinspires.ftc.teamcode.Subsystems.MecanumDrivetrain;
 
+=======
+import Subsystems.MecanumDrivetrain;
+>>>>>>> Stashed changes
 @TeleOp(name = "Drive For Now")
 public class MyTeleOp extends LinearOpMode {
+
     @Override
     public void runOpMode() {
+
         MecanumDrivetrain drivetrain = new MecanumDrivetrain(hardwareMap);
         GamepadEx driverOp = new GamepadEx(gamepad1);
 
@@ -18,11 +24,17 @@ public class MyTeleOp extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            drivetrain.drive(
-                    driverOp.getLeftX(),
-                    -driverOp.getLeftY(),
-                    driverOp.getRightX()
-            );
+            driverOp.readButtons();
+            double x = driverOp.getLeftX();
+            double y = -driverOp.getLeftY();
+            double turn = driverOp.getRightX();
+            drivetrain.drive(x, y, turn);
+
+            telemetry.addData("Drive", "%.2f, %.2f, %.2f", x, y, turn);
+            telemetry.update();
+
+            idle();
         }
+        drivetrain.drive(0, 0, 0);
     }
 }
