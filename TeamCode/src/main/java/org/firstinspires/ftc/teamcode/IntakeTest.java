d41e85ad78bf5663421de9bf7d7d78a435e90279
yash <yashf1geek@gmail.com>
2025-10-30 18:34:11 -0400

    intake code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeTest.java
@@ -4,36 +4,45 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 @TeleOp
 public class IntakeTest extends LinearOpMode {
     private DcMotorEx motor;
-    private Servo servo;
-    private double targetVelocity = 100;
-    private double servoPosition = 0.5;
+    private CRServo crservo;
+    private CRServo crservo1;
+    private CRServo crservo2;
+    private int targetDistance = 50;
     @Override
     public void runOpMode() {
         motor = hardwareMap.get(DcMotorEx.class, "intakemotor");
-        servo = hardwareMap.get(Servo.class, "intakeservo");
+        crservo = hardwareMap.get(CRServo.class, "intakeservo");
+        crservo1 = hardwareMap.get(CRServo.class, "intakeservo1");
+        crservo2 = hardwareMap.get(CRServo.class, "intakeservo2");
 
         motor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
-        servo.setPosition(servoPosition);
+        crservo.setPower(0);
 
         while (opModeIsActive()) {
-            if(gamepad1.aWasReleased()) targetVelocity += 50;
-            if(gamepad1.bWasReleased()) targetVelocity -= 50;
-            motor.setVelocity(targetVelocity);
+            if(gamepad1.aWasReleased()) {targetDistance = 50;}
+            if(gamepad1.bWasReleased()) {targetDistance = 0;}
+            motor.setTargetPosition(targetDistance);
 
-            if(gamepad1.xWasReleased()) servoPosition += 0.0075;
-            if(gamepad1.yWasReleased()) servoPosition -= 0.0075;
-            servo.setPosition(servoPosition);
+            while (gamepad1.right_trigger > 0.001) {
+                crservo.setPower(1);
+                crservo1.setPower(1);
+                crservo2.setPower(1);
+            }
+//            if(gamepad1.rightBumperWasReleased()) {crservo1.setPower(1);}
+//            if(gamepad1.leftBumperWasReleased()) {crservo1.setPower(0);}
+//            if(gamepad1.rightBumperWasReleased()) {crservo2.setPower(1);}
+//            if(gamepad1.leftBumperWasReleased()) {crservo2.setPower(0);}
 
-            telemetry.addData("Target Velocity", targetVelocity);
+            telemetry.addData("Target Position", targetDistance);
             telemetry.addData("Current Velocity", motor.getVelocity());
-            telemetry.addData("Servo Position", servoPosition);
+            telemetry.addData("Servo Power", crservo.getPower());
             telemetry.update();
         }
         motor.setVelocity(0);
