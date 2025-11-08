fcf65611c5d26b884ce7ccac04dd8fc3fc919f4b
yash <yashf1geek@gmail.com>
2025-11-08 10:12:18 -0500

    teleope name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeTest.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.CRServo;
-@TeleOp
+@TeleOp (name = "IntakeTest")
 public class IntakeTest extends LinearOpMode {
     private DcMotor motor;
     private CRServo crservo;
