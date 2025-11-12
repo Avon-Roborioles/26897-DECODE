21ed9fd0476a116210da0f65abb5ca2ca185ea3e
CheckmateKevinL <145855134+CheckmateKevinL@users.noreply.github.com>
2025-11-11 19:05:07 -0500

    shootertesting is changed

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/IntakeCommand.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.Commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.Subsystems.IntakeSubsystem;
+
+public class IntakeCommand extends CommandBase {
+
+    private IntakeSubsystem intakeSubsystem;
+
+    public IntakeCommand (IntakeSubsystem intakeSubsystem) {
+        this.intakeSubsystem = intakeSubsystem;
+    }
+
+    @Override
+    public void execute() {
+        intakeSubsystem.runMotor();
+    }
+    @Override
+    public void end(boolean interrupted) {
+        intakeSubsystem.stopMotor();
+    }
+
+}
