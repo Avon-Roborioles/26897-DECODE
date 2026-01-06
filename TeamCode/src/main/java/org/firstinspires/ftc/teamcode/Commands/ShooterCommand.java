911fc73c10eebc726c8177253299db3bcf22d74e
yash <yashf1geek@gmail.com>
2026-01-06 18:58:32 -0500

    shooter command made by me

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/ShooterCommand.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.Commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.Subsystems.ShooterSubsystem;
+
+public class ShooterCommand extends CommandBase {
+
+    private ShooterSubsystem shooterSubsystem;
+
+    public ShooterCommand (ShooterSubsystem shooterSubsystem) {
+        this.shooterSubsystem = shooterSubsystem;{
+        }
+    }
+    @Override
+    public void execute() {
+        shooterSubsystem.runAutoVelocity();
+    }
+}
